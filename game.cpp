e1db3e6c787994aa67f3ad5814a622f109aa9562
sablastBF <filip.barbaric99@gmail.com>
2022-02-24 12:54:42 +0100

    Dodao sam pozadinu i levele

--- game.cpp
+++ game.cpp
@@ -1,4 +1,3 @@
-#define STB_IMAGE_IMPLEMENTATION
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -6,40 +5,55 @@
 
 #include "game.hpp"
 #include "Brick.hpp"
+#include "level.hpp"
 
+#include "glm/gtc/matrix_transform.hpp"
 
 using namespace std;
 
 void initOpenGL();
 GLFWwindow*  crateWindow(unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 
 Game::Game(unsigned int width, unsigned int height){
 
-   GLFWwindow* window = crateWindow( width, height);
+    projection = glm::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.0f, 1.0f);  
+    this -> width = width;
+    this -> height = height;
+
+    GLFWwindow* window = crateWindow( width, height);
     if (!window) return ;
 
-    Brick *brk = new Brick();
-    brk ->  initBrickRender();
+    // Brick *brk = new Brick();
+    // brk -> setGame(this);
+    // brk ->  initBrickRender();
 
-    string shaderPathVS = "shaders/basic_brick.vs", shaderPathFS = "shaders/basic_brick.fs";
-    brk -> setShader(shaderPathVS, shaderPathFS);
-    string text = "wall.jpg";
-    brk -> setTexture(text);
+    // string shaderPathVS = "shaders/basic_brick.vs", shaderPathFS = "shaders/basic_brick.fs";
+    // brk -> setShader(shaderPathVS, shaderPathFS);
+    // string text = "wall.jpg";
+    // brk -> setTexture(text);
 
+    Level *first= new Level(this);
+    first -> loadLevelFromFile();
+    first -> addBrick();
+    string bacground = "background.jpg";
+    first -> setBackground(bacground, );
 
     while(!glfwWindowShouldClose(window)){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
-        brk -> draw();
-     
+        first -> draw();
+        // for (float x = 0.0f; x < width; x+= width/k){
+        //     for (float y = 0.0f; y <= height/2.0f; y+= height/(2.0f*k)){
+        //         brk -> draw(glm::vec2(x, y),glm::vec2(width/k,   height/(2.0f*k))); 
+        //     }   
+        // }
+      
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }
-
-    
+    }   
 }
 
 
@@ -58,6 +72,8 @@ GLFWwindow*  crateWindow(unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT){
         return nullptr;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -67,3 +83,7 @@ GLFWwindow*  crateWindow(unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT){
     return window;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
